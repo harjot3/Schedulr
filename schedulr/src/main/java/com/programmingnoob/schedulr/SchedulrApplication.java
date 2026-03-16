641581859122816984c2c4c2ba807ee676d3581a
harjot3 <harjotsingh208@outlook.com>
2026-03-16 00:04:27 -0400

    adding spring architecture

---
+++ schedulr/src/main/java/com/programmingnoob/schedulr/SchedulrApplication.java
@@ -0,0 +1,13 @@
+package com.programmingnoob.schedulr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulrApplication.class, args);
+	}
+
+}
---
+++ schedulr/src/test/java/com/programmingnoob/schedulr/SchedulrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingnoob.schedulr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
